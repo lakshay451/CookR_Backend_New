40977123771e6db1d17f9da4049d44c8834cba55
lakshay451 <89472581+lakshay451@users.noreply.github.com>
2023-04-10 22:07:10 +0530

    Update data.js

--- data.js
+++ data.js
@@ -1,5 +1,6 @@
 const mongoose = require("mongoose");
-mongoose.connect("mongodb://localhost:27017/Cookr");
+const url = process.env.MONGO_URL;
+mongoose.connect("url/CookR");
 
 const data=require('./new_data.json');
 
