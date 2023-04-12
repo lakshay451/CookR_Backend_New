f6ea23767976f1590de6808000984c62710e5798
lakshay451 <89472581+lakshay451@users.noreply.github.com>
2023-04-12 23:00:19 +0530

    Update data.js

--- data.js
+++ data.js
@@ -1,11 +1,13 @@
 const mongoose = require("mongoose");
-const url = process.env.MONGO_URL;
-mongoose.connect("url/CookR");
+mongoose.set('strictQuery', false);
+//const url = "mongodb+srv://lakshayjoshiec20b1274:Lakshay123@cluster0.h108bsp.mongodb.net/?retryWrites=true&w=majority";
+
+mongoose.connect("mongodb+srv://lakshayjoshiec20b1274:Lakshay123@cluster0.h108bsp.mongodb.net/CookR");
+
+
+//mongoose.set('strictQuery', true);
 
 const data=require('./new_data.json');
-
-
-
 
 
 let allIngredients = [];
@@ -32,40 +34,40 @@ uniqueIngredients.forEach(value=>{
 
 
 
-const recipeSchema = {
-    name: String,
-    ingredients: Array,
-    directions: Array,
-    url: String,
-    uniqueIngredients: Array,
-    id: String,
-    likes: Number,
-    comments: Number,
-    type: String,
-    difficulty: String,
-    time: String,
-}
+// const recipeSchema = {
+//     name: String,
+//     ingredients: Array,
+//     directions: Array,
+//     url: String,
+//     uniqueIngredients: Array,
+//     id: String,
+//     likes: Number,
+//     comments: Number,
+//     type: String,
+//     difficulty: String,
+//     time: String,
+// }
 
 
-const Recipe = new mongoose.model("Recipe", recipeSchema);
+// const Recipe = new mongoose.model("Recipe", recipeSchema);
 
 
-data.forEach(recipe => {
-    const newRecipe = new Recipe({
-        name: recipe.title,
-        ingredients: recipe.ingredients,
-        directions: recipe.directions,
-        url: recipe.link,
-        uniqueIngredients: recipe.NER,
-        id: recipe.id,
-        likes: recipe.likes,
-        comments: recipe.comments,
-        type: recipe.type,
-        difficulty: recipe.difficulty,
-        time: recipe.time
-    })
-    newRecipe.save();
-});
+// data.forEach(recipe => {
+//     const newRecipe = new Recipe({
+//         name: recipe.title,
+//         ingredients: recipe.ingredients,
+//         directions: recipe.directions,
+//         url: recipe.link,
+//         uniqueIngredients: recipe.NER,
+//         id: recipe.id,
+//         likes: recipe.likes,
+//         comments: recipe.comments,
+//         type: recipe.type,
+//         difficulty: recipe.difficulty,
+//         time: recipe.time
+//     })
+//     newRecipe.save();
+// });
 
 
 
